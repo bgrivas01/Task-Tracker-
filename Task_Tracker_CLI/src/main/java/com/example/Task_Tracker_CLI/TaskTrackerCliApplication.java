bd4bfa5bcfef18ee47a9c35b226e54b035d2f895
Brayan Rivas <Bgrivas16@gmail.com>
2026-05-12 21:24:32 -0500

    Initialized Spring Shell Project

---
+++ Task_Tracker_CLI/src/main/java/com/example/Task_Tracker_CLI/TaskTrackerCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task_Tracker_CLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerCliApplication.class, args);
+	}
+
+}
