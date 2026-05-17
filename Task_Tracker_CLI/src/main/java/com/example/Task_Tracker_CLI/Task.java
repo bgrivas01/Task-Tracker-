335a3c8ebb6b1eac294fcf102786e83848500105
Brayan Rivas <164957185+bgrivas01@users.noreply.github.com>
2026-05-17 14:21:35 -0500

    Added comment questions in Task.java

--- Task_Tracker_CLI/src/main/java/com/example/Task_Tracker_CLI/Task.java
+++ Task_Tracker_CLI/src/main/java/com/example/Task_Tracker_CLI/Task.java
@@ -3,20 +3,22 @@ package com.example.Task_Tracker_CLI;
 import java.time.LocalDateTime;
 
 public class Task {
-    private Integer id;
+    private Integer id;//am i able to autoincremint using @Id, would it auto increment for my json? 
     private String description;
-    private TaskStatus status;
+    private TaskStatus status; //should i remove this? and why 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public Task(Integer id, String description) { //we are going to set status to NOT_STARTED by default somehow
-        this.id = id;
+        this.id = id; 
         this.description = description;
         this.status = TaskStatus.NOT_STARTED;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
     public Task() {} 
+
+
 
     //getters and setters
     public Integer getId() {
